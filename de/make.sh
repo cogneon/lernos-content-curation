58097a6084a15a8ae3f45b51a66be03f019846be
stefandiepolder <71873224+stefandiepolder@users.noreply.github.com>
2022-07-07 13:44:11 +0200

    Content in Repository übernommen

--- de/make.sh
+++ de/make.sh
@@ -3,7 +3,9 @@ echo Starting lernOS Guide Generation ...
 
 # Variables
 filename="lernOS-Template-Guide-de"
-chapters="./src/index.md ./src/1-0-Grundlagen.md ./src/1-1-Grundidee.md ./src/1-2-Lebenszyklus.md ./src/1-3-Inhaltsverzeichnis.md ./src/1-4-Inhalt-Schreibstil.md ./src/1-5-Markdown-Syntax.md ./src/1-6-Produktionskette.md ./src/1-7-Github.md ./src/2-0-Lernpfad.md ./src/3-0-Anhang.md"
+#chapters="./src/index.md ./src/1-0-Grundlagen.md ./src/1-1-Grundidee.md ./src/1-2-Lebenszyklus.md ./src/1-3-Inhaltsverzeichnis.md ./src/1-4-Inhalt-Schreibstil.md ./src/1-5-Markdown-Syntax.md ./src/1-6-Produktionskette.md ./src/1-7-Github.md ./src/2-0-Lernpfad.md ./src/3-0-Anhang.md"
+
+chapters="./src/index.md ./src/1-0-Grundlagen.md"
 
 # Delete Old Versions
 echo Deleting old versions ...
@@ -17,22 +19,22 @@ rm -ff ../docs/de-slides/index.html
 
 # Create Microsoft Word Version (docx)
 echo Creating Word version ...
-pandoc metadata.yaml -s --resource-path="./src" -F mermaid-filter -N --toc -V lang=de-de -o $filename.docx $chapters
+pandoc metadata.yaml -s --resource-path="./src" -N --toc -V lang=de-de -o $filename.docx $chapters
 
 # Create HTML Version (html)
 echo Creating HTML version ...
-pandoc metadata.yaml -s --resource-path="./src" -F mermaid-filter -N --toc -V lang=de-de -o $filename.html $chapters
+pandoc metadata.yaml -s --resource-path="./src" -N --toc -V lang=de-de -o $filename.html $chapters
 
 # Create PDF Version (pdf)
 #echo Creating PDF version ...
-#pandoc metadata.yaml -s --resource-path="./src" -F mermaid-filter --template lernos -N --toc -V lang=de-de -o $filename.pdf $chapters
+#pandoc metadata.yaml -s --resource-path="./src" --template lernos -N --toc -V lang=de-de -o $filename.pdf $chapters
 
 # Create eBook Versions (epub, mobi)
 #echo Creating eBook versions ...
 #magick -density 300 $filename.pdf[0] ebook-cover.jpg
 #mogrify -size 2500x2500 -resize 2500x2500 ebook-cover.jpg
 #mogrify -crop 1563x2500+102+0 ebook-cover.jpg
-#pandoc metadata.yaml -s --resource-path="./src" -F mermaid-filter --epub-cover-image=ebook-cover.jpg -N --toc -V lang=de-de -o $filename.epub $chapters
+#pandoc metadata.yaml -s --resource-path="./src" --epub-cover-image=ebook-cover.jpg -N --toc -V lang=de-de -o $filename.epub $chapters
 #ebook-convert $filename.epub $filename.mobi
 
 # Create Slides (revealjs)
